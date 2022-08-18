df15d8604dcf161895f6a98d4e0b404c05f770ab
CanK <cankalender526@gmail.com>
2022-08-18 07:54:46 +0300

    initial commit

---
+++ PicsumBet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicsumBet
+//
+//  Created by Kartega Mobil Dev 1 on 18.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
