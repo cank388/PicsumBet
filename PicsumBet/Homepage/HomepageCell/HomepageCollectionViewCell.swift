efeddfc06217a6528aa7e49df23fcc72465104e5
CanK <cankalender526@gmail.com>
2022-08-18 08:18:04 +0300

    collection view cell added.

---
+++ PicsumBet/Homepage/HomepageCell/HomepageCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomepageCollectionViewCell.swift
+//  PicsumBet
+//
+//  Created by Kartega Mobil Dev 1 on 18.08.2022.
+//
+
+import UIKit
+
+class HomepageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+}
--- PicsumBet/Homepage/HomepageViewController.swift
+++ PicsumBet/Homepage/HomepageViewController.swift
@@ -12,6 +12,21 @@ class HomepageViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.register(UINib(nibName: "HomepageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "HomepageCollectionViewCell")
     }
+    
+    
+}
+
+extension HomepageViewController:  UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomepageCollectionViewCell", for: indexPath) as! HomepageCollectionViewCell
+        return cell
+    }
+    
     
 }
