bced12602890c8f4add9410f5d6fd6262bbbe0f9
CanK <cankalender526@gmail.com>
2022-08-18 08:06:50 +0300

    homepage created, collection view added.

---
+++ PicsumBet/Homepage/HomepageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomepageViewController.swift
+//  PicsumBet
+//
+//  Created by Kartega Mobil Dev 1 on 18.08.2022.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ PicsumBet/Homepage/HomepageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomepageViewModel.swift
+//  PicsumBet
+//
+//  Created by Kartega Mobil Dev 1 on 18.08.2022.
+//
+
+import Foundation
